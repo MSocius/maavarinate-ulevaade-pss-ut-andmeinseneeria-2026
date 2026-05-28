e8b963f6ca823b4ccee4295eb1797f05cf58d0c4
MSocius <margus.soots@outlook.com>
2026-05-28 13:47:56 +0300

    Update earthquakes_alert_week.sql

--- transformations/sql/earthquakes_alert_week.sql
+++ transformations/sql/earthquakes_alert_week.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE VIEW earthquakes_alert_week AS
 SELECT
     DATE(time) AS day,
     COUNT(*) AS alert_quakes
-FROM earthquakes_raw
+FROM earthquakes
 WHERE mag >= 5.0
   AND time >= NOW() - INTERVAL '7 days'
 GROUP BY 1
