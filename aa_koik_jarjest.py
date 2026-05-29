f1499877250dcb17ce9939325f0bf473ebc548dc
MSocius <margus.soots@outlook.com>
2026-05-29 23:03:38 +0300

    Add files via upload

---
+++ aa_koik_jarjest.py
@@ -0,0 +1,20 @@
+import subprocess
+
+scripts = [
+    "pg_ingest_usgs.py",
+    "openmeteo_maav_ilm_tund_PWkoodis.py",
+    "openmeteo_maav_ilm_tund_tabel.py"
+    "day_alert.py"
+]
+
+for script in scripts:
+    print(f"\n▶ Käivitan: {script}")
+    result = subprocess.run(["python", script])
+
+    if result.returncode != 0:
+        print(f"❌ Viga skriptis: {script}. Peatan ETL-i.")
+        break
+
+    print(f"✔ Valmis: {script}")
+
+print("\n🎉 Kõik skriptid edukalt käivitatud!")
