916e9d25048eda36143ba28840b3c8baad7ad32c
MSocius <margus.soots@outlook.com>
2026-06-02 15:41:44 +0300

    Update day_alert.py

--- day_alert.py
+++ day_alert.py
@@ -9,17 +9,25 @@ import psycopg2
 import matplotlib.pyplot as plt
 
 
+# Laeme .env failist muutujad
+load_dotenv()
+
+DB_USER = os.getenv("MAAVARIN_USER")
+DB_PASSWORD = os.getenv("MAAVARIN_PW")
+DB_NAME = os.getenv("PGRES_MAAVARIN_DB")
+DB_PORT = int(os.getenv("DB_PORT_HOST"))
+USGS_URL = os.getenv("USGS_URL")
+
 # PostgreSQL ühendus
 conn = psycopg2.connect(
-    dbname="MAAVARIN_PG",
-    user="meiegrupp",
-    password="meieparool",
+    dbname=DB_NAME,
+    user=DB_USER,
+    password=DB_PASSWORD,
     host="localhost",
-    port=55432
+    port=DB_PORT
 )
 
 cur = conn.cursor()
-
 
 
 # --- 1) Laeme SQL-faili ja loome/uuendame vaate ---
@@ -53,4 +61,3 @@ plt.show()
 
 cur.close()
 conn.close()
-
